51bb0066cb557149617b5da783c3feb38afb1729
Sun <markandlw@gmail.com>
2018-05-15 22:21:34 +0800

    feat: Implement ROT13 which will shift each character by 13 (ASCII) in a given string

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
+def rot13_parsing(text):
+    rot13_str = ""
+    for c in text:
+        if c.isalpha():
+            if c.islower():
+                char_num = ord(c) - 97
+                char_num = (char_num + 13) % 26 + 97
+            else:
+                char_num = ord(c) - 65
+                char_num = (char_num + 13) % 26 + 65
+
+            rot13_str += chr(char_num)
+        
+        else:
+            rot13_str += c
+
+    return rot13_str
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -23,6 +41,12 @@ class Handler(webapp2.RequestHandler):
 class ROT13Handler(Handler):
     def get(self):
         self.render('rot13.html')
+
+    def post(self):
+        user_text = self.request.get('text')
+        rot13_str = rot13_parsing(user_text)
+
+        self.render('rot13.html', rot13=rot13_str)
 
 app = webapp2.WSGIApplication([
     ('/rot13', ROT13Handler),
