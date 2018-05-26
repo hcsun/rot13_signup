20b907ad2c069758ec29b78408c5008706996c46
Sun <markandlw@gmail.com>
2018-05-26 10:34:26 +0800

    feat: Add blog funtion
    A practice to  interact with Datastore.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import webapp2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
+from google.appengine.ext import db
 
 def Rot13_parsing(text):
     rot13_str = ""
@@ -99,8 +100,61 @@ class WelcomeHandler(Handler):
             self.redirect('/signup')
 
 
+def BlogKey(name = "default"):
+    return db.Key.from_path('blogs', name)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str('post.html', p=self)
+
+
+class BlogFront(Handler):
+    def get(self):
+        posts = db.GqlQuery('select * from Post order by created desc limit 10')
+        self.render('front.html', posts=posts)
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=BlogKey())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render('permalink.html', post=post)
+
+class NewPost(Handler):
+    def get(self):
+        self.render('newpost.html')
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = BlogKey(), subject=subject, content=content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = 'Wrong Subject or Content'
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
 app = webapp2.WSGIApplication([
     ('/rot13', ROT13Handler),
     ('/signup', SignUpHandler),
     ('/welcome', WelcomeHandler),
+    ('/blog/?', BlogFront),
+    ('/blog/([0-9]+)', PostPage),
+    ('/blog/newpost', NewPost),
 ], debug=True)
